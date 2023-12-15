bc0bef2a93a758cfa6143a6a9e8332e449044fc6
beichooo <be.imagine.redo@gmail.com>
2023-12-15 00:11:06 -0400

    until create landing page

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function DashboardPage() {
+  return (
+    <Button
+      variant={"destructive"}
+      size={"lg"}
+    >
+      Click on me men
+    </Button>
+  );
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,6 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function LandingPage() {
+  return <p>Landing page (unprotected)</p>;
+}
